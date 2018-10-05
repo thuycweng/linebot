ec7a872a7a6b6ced9b498ee4382e037baa294714
g05350011 <g05350011@thu.edu.tw>
2018-10-05 22:03:59 +0800

    add words

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text='Nice to meet you!')
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
