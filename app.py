380663e76c4ed6cf2876438b73b90d54fcaedd24
g05350011 <g05350011@thu.edu.tw>
2018-10-05 22:07:38 +0800

    change code

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Nice to meet you!')
+    profile = line_bot_api.get_profile(user_id)
+    message = TextSendMessage(text='Nice to meet you!'+profile)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
