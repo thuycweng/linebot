37b2488a798062930af4a9a8e1600b6636577658
g05350011 <g05350011@thu.edu.tw>
2018-10-05 21:43:08 +0800

    add token..

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('S/uMyi3L9Ghrd9ZXe/u1CPnpd/mg85R7OX3MwR27RztSHN0Er/6O1jic/AvUTLwv8NX+K86AoJmwEE7z0eEGEkEt8d39PItYqgSAC3IPTSeH0Wq5b4qXoTjGE0ep1l4C0wPJIDwBmJQg+R+ak63o6QdB04t89/1O/w1cDnyilFU=
-')
+line_bot_api = LineBotApi('S/uMyi3L9Ghrd9ZXe/u1CPnpd/mg85R7OX3MwR27RztSHN0Er/6O1jic/AvUTLwv8NX+K86AoJmwEE7z0eEGEkEt8d39PItYqgSAC3IPTSeH0Wq5b4qXoTjGE0ep1l4C0wPJIDwBmJQg+R+ak63o6QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('8ac5c9557affc71faf51bd3ab8234e9a')
 
 
