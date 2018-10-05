3c4c7ad9b8c6476f54450a2b35b1dd29210a87c6
g05350011 <g05350011@thu.edu.tw>
2018-10-05 21:46:03 +0800

    add a )

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='Nice to meet you, ')
+        TextSendMessage(text='Nice to meet you, '))
 
 
 if __name__ == "__main__":
