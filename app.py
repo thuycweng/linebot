685cae6685ea464a2a0141fa69ad4f4d0566d052
thuycweng <43877709+thuycweng@users.noreply.github.com>
2018-10-08 16:30:44 +0800

    clean code

--- app.py
+++ app.py
@@ -43,10 +43,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #profile = line_bot_api.get_profile('<userId>')
     message = TextSendMessage(text='Nice to meet you!')
     line_bot_api.reply_message(event.reply_token, message)
-    #line_bot_api.reply_message(event.reply_token, profile.user_id)
 
 import os
 if __name__ == "__main__":
